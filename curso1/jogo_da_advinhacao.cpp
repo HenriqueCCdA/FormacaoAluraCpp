4987bcbe632b06ff98595a8844eee11e5e1b197b
Henrique C. C. de Andrade <henrique.ccda@gmail.com>
2023-12-23 20:04:38 -0300

    m

---
+++ curso1/jogo_da_advinhacao.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    cout << "***************************************" << endl;
+    cout << "* Bem-vindos aao jogo da adivinhação! *" << endl;
+    cout << "***************************************" << endl;
+}
