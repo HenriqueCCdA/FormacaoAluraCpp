a154d12ee7c9f6fcf9676e63ecd1a49d72408243
Henrique C. C. de Andrade <henrique.ccda@gmail.com>
2023-12-25 07:56:08 -0300

    m

--- curso1/jogo_da_advinhacao.cpp
+++ curso1/jogo_da_advinhacao.cpp
@@ -6,4 +6,21 @@ int main() {
     cout << "***************************************" << endl;
     cout << "* Bem-vindos aao jogo da adivinhação! *" << endl;
     cout << "***************************************" << endl;
+
+    int numero_secreto = 42;
+
+    int chute;
+    cout << "Qual seu chute ? ";
+    cin >> chute;
+    cout << "O valor do seu chute é: " << chute << endl;
+
+    if(chute == numero_secreto) {
+        cout << "Parabéns! VocÊ acertou o número sercreto!" << endl;
+    } else if(chute > numero_secreto) {
+        cout << "Seu chute foi maior que o número secreto!" << endl;
+    } else {
+        cout << "Seu chuto foi menor que o número secreto!" << endl;
+    }
+
+
 }
