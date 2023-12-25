9f47e69ce18c3bcfd2b0af645d50173fb0ec3de0
Henrique C. C. de Andrade <henrique.ccda@gmail.com>
2023-12-25 08:45:10 -0300

    m

--- curso1/jogo_da_advinhacao.cpp
+++ curso1/jogo_da_advinhacao.cpp
@@ -12,6 +12,8 @@ int main() {
     bool nao_acertou = true;
     int tentativas = 0;
 
+    double pontos = 1000.0;
+
     while(nao_acertou) {
         tentativas++;
         int chute;
@@ -19,13 +21,16 @@ int main() {
         cout << "Qual seu chute ? ";
         cin >> chute;
 
+        double pontos_perdidos = abs(chute - NUMERO_SECRETO) / 2.0;
+        pontos -= pontos_perdidos;
+
         cout << "O valor do seu chute é: " << chute << endl;
 
         bool acertou = chute == NUMERO_SECRETO;
         bool maior = chute > NUMERO_SECRETO;
 
         if(acertou) {
-            cout << "Parabéns! VocÊ acertou o número sercreto!" << endl;
+            cout << "Parabéns! Você acertou o número sercreto!" << endl;
             nao_acertou = false;
         } else if(maior) {
             cout << "Seu chute foi maior que o número secreto!" << endl;
@@ -36,4 +41,7 @@ int main() {
 
     cout << "Fim do Jogo!" << endl;
     cout << "Você acertou o número secreto em " << tentativas << " tentativas" << endl;
+    cout.precision(2);
+    cout << fixed;
+    cout << "Sua pontuação foi de " << pontos << " pontos." << endl;
 }
