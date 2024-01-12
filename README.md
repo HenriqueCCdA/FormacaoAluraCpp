0feec3fd4a1cb8890e4cdcf865a7d7b6cd0d891b
Henrique C. C. de Andrade <henrique.ccda@gmail.com>
2024-01-12 16:28:53 -0300

    Inicio do Curso 3 (12/01/2024)

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Formação Alura C++
 
 1. [Curso de C++: Conhecendo a linguagem e a STL](https://cursos.alura.com.br/course/cpp-introducao-a-linguagem): Inicio: 23/12/2023 - Fim: 30/12/2023
-1. [Curso de Avançando com C++: entenda melhor a linguagem](https://cursos.alura.com.br/course/c-plus-plus-avancando-linguagem): Inicio: 30/12/2023 - Fim: 12/01/2024
+2. [Curso de Avançando com C++: entenda melhor a linguagem](https://cursos.alura.com.br/course/c-plus-plus-avancando-linguagem): Inicio: 30/12/2023 - Fim: 12/01/2024
+3. [Curso de Orientação a Objetos com C++: Classes, métodos e atributos](https://cursos.alura.com.br/course/c-plus-plus-orientacao-objetos-classes-metodos-atributos): Inicio: 12/01/2024 - Fim: 00/00/0000
---
+++ curso3/main.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+struct Conta
+{
+    string numero;
+    string cpfTitular;
+    string nomeTitular;
+    float saldo;
+
+};
+
+void sacar(Conta& conta, float valorASacar)
+{
+    if(valorASacar < 0) {
+        cout << "Não pode sacar valor negativo" << endl;
+        return;
+    }
+
+    if(valorASacar > conta.saldo) {
+        cout << "Saldo insuficiente" << endl;
+        return;
+    }
+    conta.saldo -= valorASacar;
+}
+
+void depositar(Conta& conta, float valorADepositar)
+{
+    conta.saldo += valorADepositar;
+}
+
+int main() {
+
+    Conta umaConta;
+    umaConta.numero = "123456";
+    umaConta.cpfTitular = "123.456.789-10";
+    umaConta.nomeTitular = "Vinicius";
+    umaConta.saldo = 100;
+
+    Conta umaOutraConta;
+    umaOutraConta.saldo = 200;
+
+    depositar(umaOutraConta, 500);
+
+    cout << "Uma conta: " << umaConta.saldo << " Outra conta: " << umaOutraConta.saldo << endl;
+
+    return 0;
+}
