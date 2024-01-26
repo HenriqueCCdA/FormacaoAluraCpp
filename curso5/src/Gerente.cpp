84ec26be0f25b31322897e166ee68c1e7d1cd67e
Henrique C. C. de Andrade <henrique.ccda@gmail.com>
2024-01-26 17:28:28 -0300

    m

---
+++ curso5/src/Gerente.cpp
@@ -0,0 +1,12 @@
+#include "Gerente.hpp"
+#include "DiaDaSemana.hpp"
+
+Gerente::Gerente(Cpf cpf, std::string nome, float salario, DiaDaSemana diaDoPagamento, std::string senha)
+    : Funcionario(cpf, nome, salario, diaDoPagamento), Autenticavel(senha)
+{
+}
+
+float Gerente::bonificacao() const
+{
+    return recuperaSalario() * 0.5;
+}
