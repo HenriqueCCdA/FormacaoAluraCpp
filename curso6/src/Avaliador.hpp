73987b5ecd085747a27f8d90db307c6bbd58e397
Henrique C. C. de Andrade <henrique.ccda@gmail.com>
2024-02-01 15:35:13 -0300

    m

--- curso6/src/Avaliador.hpp
+++ curso6/src/Avaliador.hpp
@@ -1,15 +1,21 @@
 #ifndef Avaliador_hpp
 #define Avaliador_hpp
+#include <vector>
 #include "Leilao.hpp"
 
 class Avaliador
 {
 private:
-    float maiorValor;
+    float maiorValor = INT32_MIN;
+    float menorValor = INT32_MAX;
+    std::vector<Lance> maiores3Lances;
+    static bool ordenaLances(const Lance&, const Lance&);
 
 public:
     void avalia(Leilao);
     float recuperaMaiorValor() const;
+    float recuperaMenorValor() const;
+    std::vector<Lance> recupera3MaioresLances() const;
 };
 
 #endif
--- curso6/tests/teste-avaliador.cpp
+++ curso6/tests/teste-avaliador.cpp
@@ -1,7 +1,10 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
 #include "Avaliador.hpp"
-#include <iostream>
 
-int main() {
+
+TEST_CASE("Deve recuperar maior lance de leilão e ordem crescente"){
+    // Arrenge - Given
     Lance primeiroLance(Usuario("Vinicius Dias"), 1000);
     Lance segundoLance(Usuario("Ana Maria"), 2000);
     Leilao leilao("Fia 147 0km");
@@ -9,9 +12,63 @@ int main() {
     leilao.recebeLance(segundoLance);
 
     Avaliador leiloeiro;
+
+    // Act - When
     leiloeiro.avalia(leilao);
 
-    std::cout << leiloeiro.recuperaMaiorValor() << std::endl;
+    // Assert - Then
+    REQUIRE(2000 == leiloeiro.recuperaMaiorValor());
+}
 
-    return 0;
+TEST_CASE("Deve recuperar maior lance de leilão e ordem decrescente"){
+    // Arrenge - Given
+    Lance primeiroLance(Usuario("Vinicius Dias"), 2000);
+    Lance segundoLance(Usuario("Ana Maria"), 1000);
+    Leilao leilao("Fia 147 0km");
+    leilao.recebeLance(primeiroLance);
+    leilao.recebeLance(segundoLance);
+
+    Avaliador leiloeiro;
+
+    // Act - When
+    leiloeiro.avalia(leilao);
+
+    // Assert - Then
+    REQUIRE(2000 == leiloeiro.recuperaMaiorValor());
+}
+
+
+TEST_CASE("Deve recuperar menor lance de leilão e ordem decrescente"){
+    // Arrenge - Given
+    Lance primeiroLance(Usuario("Vinicius Dias"), 2000);
+    Lance segundoLance(Usuario("Ana Maria"), 1000);
+    Leilao leilao("Fia 147 0km");
+    leilao.recebeLance(primeiroLance);
+    leilao.recebeLance(segundoLance);
+
+    Avaliador leiloeiro;
+
+    // Act - When
+    leiloeiro.avalia(leilao);
+
+    // Assert - Then
+    REQUIRE(1000 == leiloeiro.recuperaMenorValor());
+}
+
+
+TEST_CASE("Deve recuperar menor lance de leilão e ordem crescente"){
+    // Arrenge - Given
+    Lance primeiroLance(Usuario("Vinicius Dias"), 1000);
+    Lance segundoLance(Usuario("Ana Maria"), 2000);
+    Leilao leilao("Fia 147 0km");
+    leilao.recebeLance(primeiroLance);
+    leilao.recebeLance(segundoLance);
+
+    Avaliador leiloeiro;
+
+    // Act - When
+    leiloeiro.avalia(leilao);
+
+    // Assert - Then
+    REQUIRE(1000 == leiloeiro.recuperaMenorValor());
 }
