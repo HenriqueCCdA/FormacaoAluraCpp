1d98058ab5797f905394f5e6e3554755475dcbc0
Henrique C. C. de Andrade <henrique.ccda@gmail.com>
2024-02-08 14:27:51 -0300

    Fim do curso 7 (08/02/2024).

--- curso7/main.cpp
+++ curso7/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <memory>
+#include <cstring>
 #include "Usuario.hpp"
 
 // void * operator new(size_t bytes)
@@ -19,6 +20,11 @@ void ExibeNomeUsuario(std::shared_ptr<Usuario> usuario)
     std::cout << usuario->recuperaNome() << std::endl;
 }
 
+int recuperaNumero()
+{
+    return 10;
+}
+
 int main2() {
     std::cout << "---------------------------" << std::endl;
     std::string casal = "Carlos Vinicius dos Santos Dias & Patricia Feiras Graça";
@@ -35,5 +41,65 @@ int main2() {
     ExibeNomeUsuario(usuario);
 
     // delete usuario;
+    return 0;
+}
+
+class String
+{
+private:
+    char* data;
+    size_t size;
+public:
+    String(const char* string)
+    {
+        std::cout << "String criada" << std::endl;
+        size = strlen(string);
+        data = new char[size];
+        data[size] = '\0';
+        memcpy(data, string, size);
+    }
+
+    String(const String& outraString)
+    {
+        std::cout << "String copiada" << std::endl;
+        size = strlen(outraString.data);
+        data = new char[size];
+        memcpy(data, outraString.data, size);
+    }
+
+    String(String && outraString)
+    {
+        std::cout << "String movida" << std::endl;
+        size = outraString.size;
+        data = outraString.data;
+
+        outraString.size = 0;
+        outraString.data = nullptr;
+    }
+
+    ~String()
+    {
+        delete data;
+    }
+};
+
+class User
+{
+private:
+    String nome;
+public:
+    User(const String& string) : nome(string)
+    {
+    }
+
+    User(String&& string) : nome(std::move(string))
+    {
+
+    }
+
+};
+
+int main() {
+    User("Vinicius");
     return 0;
 }
